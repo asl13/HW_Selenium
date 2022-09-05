dc0d7b8d6cb8c586da719df08d7c512de0b85c85
asl13 <alliskov@yandex.ru>
2022-09-05 21:21:05 +0700

    added variables, changed assertions to soft assertions, moved .gitignore from .idea directory

--- src/test/java/Homework/DemoQAPracticeFormCheck.java
+++ src/test/java/Homework/DemoQAPracticeFormCheck.java
@@ -1,8 +1,8 @@
 package Homework;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -37,6 +37,7 @@ public class DemoQAPracticeFormCheck {
         }
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
+
     }
 
     @AfterEach
@@ -47,84 +48,82 @@ public class DemoQAPracticeFormCheck {
     @Test
     public void fillInPracticeForm() {
 
+        SoftAssertions softAssert = new SoftAssertions();
+
+        String fName = "Ivan";
+        String lName = "Ivanov";
+        String email = "ivanov@ru.ru";
+        String gender = "Male";
+        String mobileNum = "0123456789";
+        String birthDate = "01 November,2000";
+        String subject = "Arts";
+        String hobby1 = "Sports";
+        String hobby2 = "Music";
+        String testPic = "test.png";
+        String homeAddress = "Novosibirsk";
+        String state = "NCR";
+        String city = "Delhi";
+
         WebElement inputFName = driver.findElement(By.cssSelector("#firstName"));
-        inputFName.sendKeys("Ivan");
+        inputFName.sendKeys(fName);
 
         WebElement inputLName = driver.findElement(By.cssSelector("#lastName"));
-        inputLName.sendKeys("Ivanov");
+        inputLName.sendKeys(lName);
 
         WebElement inputEmail = driver.findElement(By.cssSelector("#userEmail"));
-        inputEmail.sendKeys("ivanov@ru.ru");
+        inputEmail.sendKeys(email);
 
-        WebElement inputGender = driver.findElement(By.cssSelector("label[for='gender-radio-1']"));
+        WebElement inputGender = driver.findElement(By.xpath("//label[text()='" + gender + "']"));
         inputGender.click();
 
         WebElement inputMobile = driver.findElement(By.cssSelector("#userNumber"));
-        inputMobile.sendKeys("0123456789");
+        inputMobile.sendKeys(mobileNum);
 
         WebElement inputDateOfBirth = driver.findElement(By.cssSelector("#dateOfBirthInput"));
-        inputDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"), "01 Nov 2000", Keys.ENTER);
+        inputDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"), birthDate, Keys.ENTER);
 
         WebElement inputSubject = driver.findElement(By.cssSelector("#subjectsInput"));
-        inputSubject.sendKeys("Arts");
+        inputSubject.sendKeys(subject);
 
         WebElement submitSubject = driver.findElement(By.cssSelector("#react-select-2-option-0"));
         submitSubject.click();
 
-        WebElement inputHobbiesSport = driver.findElement(By.cssSelector("label[for='hobbies-checkbox-1'"));
+        WebElement inputHobbiesSport = driver.findElement(By.xpath("//label[text()='" + hobby1 + "']"));
         inputHobbiesSport.click();
-        WebElement inputHobbiesMusic = driver.findElement(By.cssSelector("label[for='hobbies-checkbox-3'"));
+        WebElement inputHobbiesMusic = driver.findElement(By.xpath("//label[text()='" + hobby2 + "']"));
         inputHobbiesMusic.click();
 
         WebElement inputAddress = driver.findElement(By.cssSelector("#currentAddress"));
-        inputAddress.sendKeys("Novosibirsk");
+        inputAddress.sendKeys(homeAddress);
 
         WebElement uploadPicture = driver.findElement(By.cssSelector("#uploadPicture"));
-        uploadPicture.sendKeys(System.getProperty("user.dir")+"\\src\\img\\test.png");
+        uploadPicture.sendKeys(System.getProperty("user.dir") + "\\src\\img\\" + testPic);
 
         WebElement inputState = driver.findElement(By.cssSelector("#react-select-3-input"));
-        inputState.sendKeys("NCR");
+        inputState.sendKeys(state);
 
         WebElement submitState = driver.findElement(By.cssSelector("#react-select-3-option-0"));
         submitState.click();
 
         WebElement inputCity = driver.findElement(By.cssSelector("#react-select-4-input"));
-        inputCity.sendKeys("Delhi");
+        inputCity.sendKeys(city);
 
         WebElement sumbitCity = driver.findElement(By.cssSelector("#react-select-4-option-0"));
         sumbitCity.click();
         inputCity.submit();
 
-        WebElement studentName = driver.findElement(By.xpath("//td[text()='Student Name']/../td[2]"));
-        Assertions.assertEquals("Ivan Ivanov", studentName.getText());
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Student Name']/../td[2]")).getText()).isEqualTo(fName + " " + lName);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Student Email']/../td[2]")).getText()).isEqualTo(email);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Gender']/../td[2]")).getText()).isEqualTo(gender);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Mobile']/../td[2]")).getText()).isEqualTo(mobileNum);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Date of Birth']/../td[2]")).getText()).isEqualTo(birthDate);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Subjects']/../td[2]")).getText()).isEqualTo(subject);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Hobbies']/../td[2]")).getText()).isEqualTo(hobby1 + ", " + hobby2);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Picture']/../td[2]")).getText()).isEqualTo(testPic);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='Address']/../td[2]")).getText()).isEqualTo(homeAddress);
+        softAssert.assertThat(driver.findElement(By.xpath("//td[text()='State and City']/../td[2]")).getText()).isEqualTo(state + " " + city);
 
-        WebElement studentEmail = driver.findElement(By.xpath("//td[text()='Student Email']/../td[2]"));
-        Assertions.assertEquals("ivanov@ru.ru", studentEmail.getText());
-
-        WebElement gender = driver.findElement(By.xpath("//td[text()='Gender']/../td[2]"));
-        Assertions.assertEquals("Male", gender.getText());
-
-        WebElement mobile = driver.findElement(By.xpath("//td[text()='Mobile']/../td[2]"));
-        Assertions.assertEquals("0123456789", mobile.getText());
-
-        WebElement dateOfBirth = driver.findElement(By.xpath("//td[text()='Date of Birth']/../td[2]"));
-        Assertions.assertEquals("01 November,2000", dateOfBirth.getText());
-
-        WebElement subjects = driver.findElement(By.xpath("//td[text()='Subjects']/../td[2]"));
-        Assertions.assertEquals("Arts", subjects.getText());
-
-        WebElement hobbies = driver.findElement(By.xpath("//td[text()='Hobbies']/../td[2]"));
-        Assertions.assertEquals("Sports, Music", hobbies.getText());
-
-        WebElement picture = driver.findElement(By.xpath("//td[text()='Picture']/../td[2]"));
-        Assertions.assertEquals("test.png", picture.getText());
-
-        WebElement address = driver.findElement(By.xpath("//td[text()='Address']/../td[2]"));
-        Assertions.assertEquals("Novosibirsk", address.getText());
-
-        WebElement stateAndCity = driver.findElement(By.xpath("//td[text()='State and City']/../td[2]"));
-        Assertions.assertEquals("NCR Delhi", stateAndCity.getText());
-
+        softAssert.assertAll();
     }
 
 }
